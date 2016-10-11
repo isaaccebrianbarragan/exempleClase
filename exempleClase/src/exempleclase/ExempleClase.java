6bb526cf97f4a5ff81fecbee99704944ac2e7ace
isaaccebrianbarragan <isaaccebrianbarragan@gmail.com>
2016-10-11 16:31:59 +0200

    First commit from github editor
    First commit created in github editor.

--- exempleClase/src/exempleclase/ExempleClase.java
+++ exempleClase/src/exempleclase/ExempleClase.java
@@ -10,7 +10,7 @@ public class ExempleClase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hello from github");
     }
     
 }
