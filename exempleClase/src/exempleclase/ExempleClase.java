9fe1d6fb18594ab4426c63bd7c26383163037224
isaac <isaac@isaac-VirtualBox>
2016-10-11 16:16:43 +0200

    First commit in ID Netbeans.

---
+++ exempleClase/src/exempleclase/ExempleClase.java
@@ -0,0 +1,16 @@
+package exempleclase;
+
+/**
+ *
+ * @author isaac
+ */
+public class ExempleClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
